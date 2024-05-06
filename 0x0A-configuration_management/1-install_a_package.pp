b63a8efaaf339c289a5ab69424f319fd497b430c
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-05-06 09:12:11 -0700

    Fix: Add Werkzeug package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Install flask 2.1.0
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
-  name     => 'flask',
+  name     => 'Flask',
+  provider => 'pip3'
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
