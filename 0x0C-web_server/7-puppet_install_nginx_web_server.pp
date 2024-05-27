24af63fc2ba9ffee7ee9866cd8e1dad08ff4cf2c
Alphonse <alphonsemehounme9gmail.com>
2024-05-27 03:55:42 -0700

    Update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,7 +15,7 @@ exec {'index':
 }
 
 exec {'config':
-  command  =>'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/google.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/google.com\/;\\n\\t}/" /etc/nginx/sites-available/default',
   provider => shell,
 }
 
