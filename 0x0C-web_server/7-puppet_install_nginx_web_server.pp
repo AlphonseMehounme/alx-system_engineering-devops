a37070bd2803ba77e13d116490a0888fa95d3c71
Alphonse <alphonsemehounme9gmail.com>
2024-05-27 03:40:23 -0700

    update 'shell' to shell in script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,31 +6,20 @@ package {'nginx':
 
 exec {'install':
   command  => 'sudo apt-get update; sudo apt-get -y install nginx',
-  provider => 'shell',
+  provider => shell,
 }
 
 exec {'index':
   command  => 'echo "hello world" | sudo tee /var/www/html/index.html',
-  provider => 'shell',
+  provider => shell,
 }
 
 exec {'config':
   command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/google.com\/;\\n\\t}/" /etc/nginx/sites-available/default',
-  provider => 'shell',
+  provider => shell,
 }
 
 exec {'reload':
-  command  => 'service nginx reload',
-  provider => 'shell',
+  command  => 'sudo service nginx reload',
+  provider => shell,
 }
-
-#exec {'config':
-#  commande => 'printf %s "server {
-#        listen 80 default_server;
-#        listen [::]:80 default_server;
-#        root /var/www/html;
-#        index index.html index.htm index.nginx-debian.html;
-#        location /redirect_me {
-#                return 301 https://www.google.com;
-#        }
-#}" > /etc/nginx/sites-available/default',
