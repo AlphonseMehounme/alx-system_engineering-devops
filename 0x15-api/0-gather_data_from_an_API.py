7c707267ebe73a70b5cdb342b0eef9a2d959fe6b
Alphonse <alphonsemehounme9gmail.com>
2024-06-08 07:36:47 -0700

    Request and print employee todo list

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module list employee details
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    theid = sys.argv[1]
+    empurl = "https://jsonplaceholder.typicode.com/users/" + theid
+    todosurl = "https://jsonplaceholder.typicode.com/todos/"
+    emp = requests.get(empurl).json()
+    EMPLOYEE_NAME = emp['name']
+    params = {'userId': theid}
+    todos = requests.get(todosurl, params=params).json()
+    TOTAL_NUMBER_OF_TASKS = len(todos)
+    params = {'userId': theid, 'completed': 'true'}
+    oktodos = requests.get(todosurl, params=params).json()
+    NUMBER_OF_DONE_TASKS = len(oktodos)
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks\
+            ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    for todo in oktodos:
+        print(f"\t {todo['title']}")
