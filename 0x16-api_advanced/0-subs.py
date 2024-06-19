05edbf3d8a51a986091c1022c81230256af3f02b
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-06-19 03:32:00 +0100

    Request to reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module function returns the number of subs of a subreddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Return number of subs
+    """
+    url = "https://www.reddit.com/api/v1/r/" + subreddit + "/about/"
+    res = requests.get(url)
+    print(res)
+    return 1000
+    
